e864b4909b81440b1210e32e4b4f594f16a83b15
espositofederico44 <92693588+espositofederico44@users.noreply.github.com>
2022-04-21 10:23:44 +0200

    start

---
+++ bot_telegram.js
@@ -0,0 +1,41 @@
+import TelegramBot from 'node-telegram-bot-api';
+import getIdOfTheDay from "./pokemon.js";
+import fetch from 'node-fetch';
+
+const token = '';
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/pokemon/, async (msg, match) => {
+
+  const chatId = msg.chat.id;
+  const id = getIdOfTheDay();
+  
+  // http get api per prendere info
+  // https://pokeapi.co/api/v2/pokemon/ +  id
+  const req = "https://pokeapi.co/api/v2/pokemon/" +  id
+  // embed nel messaggio 
+  // invio
+
+  const res = await fetch(req)
+                            .then(response => response.json())
+                            .then(data => {
+                              // const message = JSON.stringify(data)
+                              // console.log(message)
+                              // invia il messaggio
+                              var message = `Today's pokemon is the number ${data.id} - ${data.name}`
+                              const photoURL = data.sprites.other["official-artwork"].front_default
+															message += "\ntypes:"
+															data.types.forEach(element => {
+																console.log(element)
+																message += " " + element.type.name
+															});
+                              // bot.sendMessage(chatId, message);
+                              bot.sendPhoto(chatId, photoURL, {caption: message});
+															// per i tipi mettiamo per ogni tipo un'emoji
+                            });
+});
+
+bot.on('polling_error', (error) => {
+	console.log(error);  // => 'EFATAL'
+});
