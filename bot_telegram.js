ce856cfb27ac3c90e8ace16856ba185654c02aea
Alessandro Bordonali <alessandro.bordo41@gmail.com>
2022-04-21 11:22:11 +0200

    prima branching

--- bot_telegram.js
+++ bot_telegram.js
@@ -1,41 +1,42 @@
-import TelegramBot from 'node-telegram-bot-api';
+import TelegramBot from "node-telegram-bot-api";
 import getIdOfTheDay from "./pokemon.js";
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
+import 'dotenv/config'
 
-const token = '';
+const token = process.env.TELEGRAM_TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/pokemon/, async (msg, match) => {
-
   const chatId = msg.chat.id;
   const id = getIdOfTheDay();
-  
+
   // http get api per prendere info
   // https://pokeapi.co/api/v2/pokemon/ +  id
-  const req = "https://pokeapi.co/api/v2/pokemon/" +  id
-  // embed nel messaggio 
+  const req = "https://pokeapi.co/api/v2/pokemon/" + id;
+  // embed nel messaggio
   // invio
 
   const res = await fetch(req)
-                            .then(response => response.json())
-                            .then(data => {
-                              // const message = JSON.stringify(data)
-                              // console.log(message)
-                              // invia il messaggio
-                              var message = `Today's pokemon is the number ${data.id} - ${data.name}`
-                              const photoURL = data.sprites.other["official-artwork"].front_default
-															message += "\ntypes:"
-															data.types.forEach(element => {
-																console.log(element)
-																message += " " + element.type.name
-															});
-                              // bot.sendMessage(chatId, message);
-                              bot.sendPhoto(chatId, photoURL, {caption: message});
-															// per i tipi mettiamo per ogni tipo un'emoji
-                            });
+    .then((response) => response.json())
+    .then((data) => {
+      // const message = JSON.stringify(data)
+      // console.log(message)
+      // invia il messaggio
+      var message = `Today's pokemon is the number ${data.id} - ${data.name}`;
+      const photoURL =
+        data.sprites.other["official-artwork"].front_default;
+      message += "\ntypes:";
+      data.types.forEach((element) => {
+        console.log(element);
+        message += " " + element.type.name;
+      });
+      // bot.sendMessage(chatId, message);
+      bot.sendPhoto(chatId, photoURL, { caption: message });
+      // per i tipi mettiamo per ogni tipo un'emoji
+    });
 });
 
-bot.on('polling_error', (error) => {
-	console.log(error);  // => 'EFATAL'
+bot.on("polling_error", (error) => {
+  console.log(error); // => 'EFATAL'
 });
